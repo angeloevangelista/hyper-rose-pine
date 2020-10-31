3339e6f7ff77289aae6acbf2f1d9e711e07245f1
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-10-31 12:26:20 -0300

    Changing border color

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const colors = {
   blue: "#31748f",
   lightBlue: "#31748f", // normal folders: font color
   cyan: "#7cc2c2",
-  lightCyan: "#afeeee", // symlink folders: font color 
+  lightCyan: "#afeeee", // symlink folders: font color
   green: "#8bd995", // folders background
   lightGreen: "#c7edd4",
   magenta: "#9f88e3", // correct directory
@@ -14,15 +14,16 @@ const colors = {
   red: "#c25977",
   lightRed: "#eb6f92",
   yellow: "#f0bf7a",
-  lightYellow: "#f7d29e"
+  lightYellow: "#f7d29e",
 };
-exports.decorateConfig = config =>
+
+exports.decorateConfig = (config) =>
   Object.assign({}, config, {
     padding: "30px 30px 20px 30px",
     backgroundColor: "#191724",
     foregroundColor: "#e0def4",
     cursorColor: "#bab9c4",
     selectionColor: "rgba(112, 110, 134, 0.2)",
-    borderColor: "#0000",
-    colors
+    borderColor: "#242136", // it is also the scroll color :/
+    colors,
   });
